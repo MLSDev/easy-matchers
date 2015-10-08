512b23dfac0c05f4b3c4321c16750994d852d610
Igor Zubkov <zubkov@mlsdev.com>
2015-10-08 18:41:00 +0300

    Fix loading

--- lib/rspec-rails-matchers.rb
+++ lib/rspec-rails-matchers.rb
@@ -1,1 +1,2 @@
+require 'rspec/rails/active_model/validations/matchers/validations'
 require 'rspec/rails/active_model/validations/matchers/presence_of'
--- lib/rspec/rails/active_model/validations/matchers/presence_of.rb
+++ lib/rspec/rails/active_model/validations/matchers/presence_of.rb
@@ -1,5 +1,3 @@
-require_relative 'validations'
-
 module RSpec
   module Rails
     module ActiveModel
