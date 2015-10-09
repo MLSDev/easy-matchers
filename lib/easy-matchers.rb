4f451f0257175898a9980910eab20439c5e5f63b
Igor Zubkov <zubkov@mlsdev.com>
2015-10-09 13:35:58 +0300

    Add message validation to validate_acceptance_of

--- lib/easy-matchers.rb
+++ lib/easy-matchers.rb
@@ -1,3 +1,4 @@
 require 'matchers/validations'
+require 'matchers/validations/with_message'
 require 'matchers/validations/presence_of'
 require 'matchers/validations/acceptance_of'
--- lib/matchers/validations/acceptance_of.rb
+++ lib/matchers/validations/acceptance_of.rb
@@ -2,7 +2,30 @@ module Easy
   module Matchers
     module Validations
       def validate_acceptance_of(field)
-        HaveValidationMatcher.new(field, :acceptance)
+        ValidateAcceptanceOfMatcher.new(field)
+      end
+
+      class ValidateAcceptanceOfMatcher < HaveValidationMatcher
+        include WithMessage
+
+        def initialize(name)
+          super(name, :acceptance)
+        end
+
+        def matches?(actual)
+          @result = super(actual)
+
+          return false unless @result
+
+          check_expected_message if @expected_message
+
+          @result
+        end
+
+        def with_message(message)
+          @expected_message = message
+          self
+        end
       end
     end
   end
