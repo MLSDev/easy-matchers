be567ed7ecf56ad5c9ad30a90ba7f1d59726db13
Igor Zubkov <zubkov@mlsdev.com>
2015-10-09 13:40:39 +0300

    Add message validation to validate_presence_of

--- lib/matchers/validations/presence_of.rb
+++ lib/matchers/validations/presence_of.rb
@@ -2,7 +2,30 @@ module Easy
   module Matchers
     module Validations
       def validate_presence_of(field)
-        HaveValidationMatcher.new(field, :presence)
+        ValidatePresenceOfMatcher.new(field)
+      end
+
+      class ValidatePresenceOfMatcher < HaveValidationMatcher
+        include WithMessage
+
+        def initialize(name)
+          super(name, :presence)
+        end
+
+        def matches?(actual)
+          @result = super(actual)
+
+          return false unless @result
+
+          check_expected_message if @expected_message
+
+          @result
+        end
+
+        def with_message(message)
+          @expected_message = message
+          self
+        end
       end
     end
   end
