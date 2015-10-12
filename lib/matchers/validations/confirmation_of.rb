1dd7d0ceac81c7231963a33e4d92023a6d63205f
Igor Zubkov <zubkov@mlsdev.com>
2015-10-12 16:33:04 +0300

    More refactoring

--- lib/matchers/validations/confirmation_of.rb
+++ lib/matchers/validations/confirmation_of.rb
@@ -1,31 +1,48 @@
 module Easy
   module Matchers
     module Validations
-      def validate_confirmation_of(field)
-        ValidateConfirmationOfMatcher.new(field)
+      def validate_confirmation_of(*attributes)
+        ValidateConfirmationOfMatcher.new(attributes)
       end
 
-      class ValidateConfirmationOfMatcher < HaveValidationMatcher
-        include WithMessage
+      class ValidateConfirmationOfMatcher < BaseValidationMatcher
+        attr_reader :confirmation_attributes
 
-        def initialize(name)
-          super(name, :confirmation)
+        def initialize(attributes)
+          super(attributes, :confirmation)
+          @confirmation_attributes = attributes
         end
 
-        def matches?(actual)
-          @result = super(actual)
+        def matches?(subject)
+          super(subject)
 
-          return false unless @result
+          matched_validator = class_name.validators.detect do |validator|
+            validator.kind == type &&
+            validator.attributes.sort == attributes.sort &&
+            validator.options.sort == options.sort
+          end
 
-          check_expected_message if @expected_message
+          true if matched_validator
+        end
 
-          @result
+        def description
+          "require #{ confirmation_attributes.join(', ') } to match #{ attributes.join(', ') }"
         end
 
         def with_message(message)
-          @expected_message = message
+          options[:message] = message
           self
         end
+
+        def on(context)
+          options[:on] = context
+          self
+        end
+
+        # TODO: add support
+        # There is also a list of default options supported by every validator:
+        # +:if+, +:unless+, +:on+, +:allow_nil+, +:allow_blank+, and +:strict+.
+        # See <tt>ActiveModel::Validation#validates</tt> for more information
       end
     end
   end
--- lib/matchers/validations/presence_of.rb
+++ lib/matchers/validations/presence_of.rb
@@ -96,6 +96,11 @@ module Easy
           options[:on] = context
           self
         end
+
+        # TODO: add support
+        # There is also a list of default options supported by every validator:
+        # +:if+, +:unless+, +:on+, +:allow_nil+, +:allow_blank+, and +:strict+.
+        # See <tt>ActiveModel::Validation#validates</tt> for more information
       end
     end
   end
