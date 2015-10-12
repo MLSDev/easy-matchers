ec90f154f9ec6d1dd20dce7a647a0284777635e3
Igor Zubkov <zubkov@mlsdev.com>
2015-10-12 17:14:39 +0300

    Refactoring

--- lib/matchers/validations/acceptance_of.rb
+++ lib/matchers/validations/acceptance_of.rb
@@ -1,30 +1,55 @@
 module Easy
   module Matchers
     module Validations
-      def validate_acceptance_of(field)
-        ValidateAcceptanceOfMatcher.new(field)
+      def validate_acceptance_of(*attributes)
+        ValidateAcceptanceOfMatcher.new(attributes)
       end
 
-      class ValidateAcceptanceOfMatcher < HaveValidationMatcher
-        include WithMessage
-
-        def initialize(name)
-          super(name, :acceptance)
+      class ValidateAcceptanceOfMatcher < BaseValidationMatcher
+        def initialize(attributes)
+          super(attributes, :acceptance)
         end
 
-        def matches?(actual)
-          @result = super(actual)
+        def matches?(subject)
+          super(subject)
 
-          return false unless @result
+          add_allow_nil_and_accept_to_options
 
-          check_expected_message if @expected_message
+          matched_validator = class_name.validators.detect do |validator|
+            validator.kind == type &&
+            validator.attributes.sort == attributes.sort &&
+            validator.options.sort == options.sort
+          end
 
-          @result
+          true if matched_validator
+        end
+
+        def description
+          "require #{ attributes.join(', ') } to be accepted"
         end
 
         def with_message(message)
-          @expected_message = message
+          options[:message] = message
           self
+        end
+
+        def on(context)
+          options[:on] = context
+          self
+        end
+
+        # TODO: add support
+        # There is also a list of default options supported by every validator:
+        # +:if+, +:unless+, +:on+, +:allow_nil+, +:allow_blank+, and +:strict+.
+        # See <tt>ActiveModel::Validation#validates</tt> for more information
+
+        private
+
+        # TODO: check :allow_nil and :accept before setting
+        # NOTE: Rails adds this to options by defaults
+        def add_allow_nil_and_accept_to_options
+          options[:allow_nil] = true
+          options[:accept] = '1'
         end
       end
     end
