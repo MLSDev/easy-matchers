b0db78d03730cb1680b76f2e2564e351f68e95c9
Igor Zubkov <zubkov@mlsdev.com>
2015-11-27 15:34:43 +0200

    Add modules

--- lib/easy-matchers.rb
+++ lib/easy-matchers.rb
@@ -5,3 +5,11 @@ require 'matchers/validations/confirmation_of'
 require 'matchers/validations/length_of'
 require 'matchers/validations/numericality_of'
 require 'matchers/validations/presence_of'
+
+module Easy
+  module Matchers
+    module Validations
+
+    end
+  end
+end
